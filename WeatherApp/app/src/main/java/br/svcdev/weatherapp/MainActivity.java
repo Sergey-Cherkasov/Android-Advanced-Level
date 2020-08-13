eef87be7d1b83da0bde7aefd2c0508f8f9f780f6
Sergey-Cherkasov <s.cherkasov_tester@yahoo.com>
2020-08-13 13:20:57 +0700

    Homework06. Реализован broadcast receiver для получения состояни сети и уровня заряда батареи. Реализовано получение push-уведомления при отправке с сервиса Firebase.

--- WeatherApp/app/src/main/java/br/svcdev/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/br/svcdev/weatherapp/MainActivity.java
@@ -1,10 +1,13 @@
 package br.svcdev.weatherapp;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,135 +23,152 @@ import br.svcdev.weatherapp.databinding.ActivityMainBinding;
 import br.svcdev.weatherapp.fragments.WeatherCurrentConditions;
 import br.svcdev.weatherapp.fragments.WeatherDailyForecast;
 import br.svcdev.weatherapp.models.WeatherAppSettings;
+import br.svcdev.weatherapp.notifications.Notifications;
+import br.svcdev.weatherapp.receivers.AppReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding mBinding;
-    private SensorManager mSensorManager;
+	private ActivityMainBinding mBinding;
 
-    private Bundle mArgs = new Bundle();
+	private SensorManager mSensorManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
+	private BroadcastReceiver networkStateReceiver = new AppReceiver();
 
-        requestSensors();
+	private Bundle mArgs = new Bundle();
 
-        loadWeatherAppSettings();
-        initAppBar();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(mBinding.getRoot());
 
-        /*
-         * Инициализован слушатель на пункты меню в navigation view. При нажатии на пункт меню,
-         * он остается выделенным.
-         */
-        mBinding.navigationView.setNavigationItemSelectedListener(item -> {
-            item.setChecked(true);
-            return true;
-        });
+		registerReceiver(networkStateReceiver,
+				new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+		registerReceiver(networkStateReceiver,
+				new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED));
+		registerReceiver(networkStateReceiver,
+				new IntentFilter(Intent.ACTION_BATTERY_LOW));
+		Notifications.initNotificationChannel(this);
 
-        initWeatherCurrentConditionsFragment();
-        initWeatherDailyForecastFragment();
-    }
+		requestSensors();
+		loadWeatherAppSettings();
+		initAppBar();
+		/*
+		 * Инициализация слушателя на пункты меню в navigation view. При нажатии на пункт меню,
+		 * он остается выделенным.
+		 */
+		mBinding.navigationView.setNavigationItemSelectedListener(item -> {
+			item.setChecked(true);
+			return true;
+		});
 
-    /**
-     * Метод инициализации сенсоров: TYPE_AMBIENT_TEMPERATURE, TYPE_RELATIVE_HUMIDITY
-     */
-    private void requestSensors() {
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) == null &&
-                mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) == null) {
-            showAlertDialog();
-            mArgs.putBoolean("Temperature", false);
-            mArgs.putBoolean("Humidity", false);
-        } else {
-            mArgs.putBoolean("Temperature", true);
-            mArgs.putBoolean("Humidity", true);
-        }
-    }
+		initWeatherCurrentConditionsFragment();
+		initWeatherDailyForecastFragment();
+	}
 
-    private void showAlertDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Attention").setMessage("There are no temperature" +
-                " or humidity sensors on Your device").setCancelable(false)
-                .setPositiveButton(R.string.menu_ok, (dialog, id) -> {
-                });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
+	/**
+	 * Метод инициализации сенсоров: TYPE_AMBIENT_TEMPERATURE, TYPE_RELATIVE_HUMIDITY
+	 */
+	private void requestSensors() {
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) == null &&
+				mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) == null) {
+			showAlertDialog();
+			mArgs.putBoolean("Temperature", false);
+			mArgs.putBoolean("Humidity", false);
+		} else {
+			mArgs.putBoolean("Temperature", true);
+			mArgs.putBoolean("Humidity", true);
+		}
+	}
 
-    private void loadWeatherAppSettings() {
-        SharedPreferences sp = WeatherAppPreferenceUtils.getPreferencesInstance(this);
-        WeatherAppSettings settings = WeatherAppSettings.getWeatherAppSettings();
-        settings.setTemperatureUnits(sp.getBoolean("temperature_units", false));
-        settings.setWindSpeedUnits(sp.getBoolean("speed_units", false));
-    }
+	private void showAlertDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Attention").setMessage("There are no temperature" +
+				" or humidity sensors on Your device").setCancelable(false)
+				.setPositiveButton(R.string.menu_ok, (dialog, id) -> {
+				});
+		AlertDialog alertDialog = builder.create();
+		alertDialog.show();
+	}
 
-    /**
-     * Метод инициализирует app bar и подключает слушателя на navigation button, при нажатии
-     * на которую открывается navigation view.
-     */
-    private void initAppBar() {
-        mBinding.componentAppBar.appBar.setNavigationIcon(R.drawable.ic_menu);
-        setSupportActionBar(mBinding.componentAppBar.appBar);
-        mBinding.componentAppBar.appBar.setNavigationOnClickListener(view -> mBinding.drawerLayout
-                .openDrawer(GravityCompat.START));
-    }
+	private void loadWeatherAppSettings() {
+		SharedPreferences sp = WeatherAppPreferenceUtils.getPreferencesInstance(this);
+		WeatherAppSettings settings = WeatherAppSettings.getWeatherAppSettings();
+		settings.setTemperatureUnits(sp.getBoolean("temperature_units", false));
+		settings.setWindSpeedUnits(sp.getBoolean("speed_units", false));
+	}
 
-    private void initWeatherDailyForecastFragment() {
-        Fragment mWeatherDailyForecastFragment = new WeatherDailyForecast();
-        mWeatherDailyForecastFragment.setArguments(mArgs);
-        FragmentTransaction mWeatherDailyForecastTransaction = getSupportFragmentManager()
-                .beginTransaction();
-        mWeatherDailyForecastTransaction.replace(R.id.fragment_weather_daily_forecast,
-                mWeatherDailyForecastFragment).commit();
-    }
+	/**
+	 * Метод инициализирует app bar и подключает слушателя на navigation button, при нажатии
+	 * на которую открывается navigation view.
+	 */
+	private void initAppBar() {
+		mBinding.componentAppBar.appBar.setNavigationIcon(R.drawable.ic_menu);
+		setSupportActionBar(mBinding.componentAppBar.appBar);
+		mBinding.componentAppBar.appBar.setNavigationOnClickListener(view -> mBinding.drawerLayout
+				.openDrawer(GravityCompat.START));
+	}
 
-    private void initWeatherCurrentConditionsFragment() {
-        Fragment mWeatherCurrentConditionsFragment = new WeatherCurrentConditions();
-        mWeatherCurrentConditionsFragment.setArguments(mArgs);
-        FragmentTransaction mWeatherCurrentConditionsTransaction = getSupportFragmentManager()
-                .beginTransaction();
-        mWeatherCurrentConditionsTransaction.replace(R.id.fragment_weather_current_conditions,
-                mWeatherCurrentConditionsFragment).commit();
-    }
+	private void initWeatherDailyForecastFragment() {
+		Fragment mWeatherDailyForecastFragment = new WeatherDailyForecast();
+		mWeatherDailyForecastFragment.setArguments(mArgs);
+		FragmentTransaction mWeatherDailyForecastTransaction = getSupportFragmentManager()
+				.beginTransaction();
+		mWeatherDailyForecastTransaction.replace(R.id.fragment_weather_daily_forecast,
+				mWeatherDailyForecastFragment).commit();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+	private void initWeatherCurrentConditionsFragment() {
+		Fragment mWeatherCurrentConditionsFragment = new WeatherCurrentConditions();
+		mWeatherCurrentConditionsFragment.setArguments(mArgs);
+		FragmentTransaction mWeatherCurrentConditionsTransaction = getSupportFragmentManager()
+				.beginTransaction();
+		mWeatherCurrentConditionsTransaction.replace(R.id.fragment_weather_current_conditions,
+				mWeatherCurrentConditionsFragment).commit();
+	}
 
-    /**
-     * Реализован метод выбора действий при нажатии на пункт меню app bar.
-     */
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.search_location:
-                runActivity(SearchLocationActivity.class);
-                break;
-            case R.id.settings:
-                runActivity(SettingsActivity.class);
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    private void runActivity(Class<?> nameClass) {
-        startActivity(new Intent(this, nameClass));
-    }
+	/**
+	 * Реализован метод выбора действий при нажатии на пункт меню app bar.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.search_location:
+				runActivity(SearchLocationActivity.class);
+				break;
+			case R.id.settings:
+				runActivity(SettingsActivity.class);
+				break;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+		return true;
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+	private void runActivity(Class<?> nameClass) {
+		startActivity(new Intent(this, nameClass));
+	}
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(networkStateReceiver);
+	}
 }
---
+++ WeatherApp/app/src/main/java/br/svcdev/weatherapp/services/WeatherAppMessagingService.java
@@ -0,0 +1,46 @@
+package br.svcdev.weatherapp.services;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import br.svcdev.weatherapp.R;
+
+public class WeatherAppMessagingService extends FirebaseMessagingService {
+
+	private int messageId = 0;
+
+	@Override
+	public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+		String title = remoteMessage.getNotification().getTitle();
+		if (title == null){
+			title = "Push Message";
+		}
+		String text = remoteMessage.getNotification().getBody();
+		NotificationCompat.Builder builder =
+				new NotificationCompat.Builder(this, "2")
+						.setSmallIcon(R.mipmap.ic_launcher)
+						.setContentTitle(title)
+						.setContentText(text);
+		NotificationManager notificationManager =
+				(NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(messageId++, builder.build());
+	}
+
+	@Override
+	public void onNewToken(@NonNull String token) {
+		Log.d("PushMessage", "Token " + token);
+		sendRegistrationToServer(token);
+	}
+
+	private void sendRegistrationToServer(String token) {
+
+	}
+
+}
